d8551972a2095c1fe8603ae7020354efcec7ebab
GeneralGak <57255134+GeneralGak@users.noreply.github.com>
2026-04-01 00:04:57 +0200

    Update Component.cs

--- SPL2_Project/GameObjects/Components/Component.cs
+++ SPL2_Project/GameObjects/Components/Component.cs
@@ -7,39 +7,39 @@ namespace SPL2_Project;
 public interface Component
 {
     /// <summary>
-        /// Is component enabled
-        /// </summary>
-        public bool IsEnabled { get; set; }
+    /// Is component enabled
+    /// </summary>
+    public bool IsEnabled { get; set; }
 
-        /// <summary>
-        /// Main GameObject reference
-        /// </summary>
-        public GameObject GameObject { get; set; }
+    /// <summary>
+    /// Main GameObject reference
+    /// </summary>
+    public GameObject GameObject { get; set; }
 
-        /// <summary>
-        /// Awake method
-        /// </summary>
-        public void Awake();
+    /// <summary>
+    /// Awake method
+    /// </summary>
+    public void Awake();
 
-        /// <summary>
-        /// Awake method
-        /// </summary>
-        public void Start();
+    /// <summary>
+    /// Awake method
+    /// </summary>
+    public void Start();
 
-        /// <summary>
-        /// Runs code and methodes every cycle.
-        /// </summary>
-        /// <param name="gameTime"></param>
-        public void Update(GameTime gameTime);
+    /// <summary>
+    /// Runs code and methodes every cycle.
+    /// </summary>
+    /// <param name="gameTime"></param>
+    public void Update(GameTime gameTime);
 
-        /// <summary>
-        /// Draws components to screen.
-        /// </summary>
-        /// <param name="spriteBatch"></param>
-        public void Draw(SpriteBatch spriteBatch);
+    /// <summary>
+    /// Draws components to screen.
+    /// </summary>
+    /// <param name="spriteBatch"></param>
+    public void Draw(SpriteBatch spriteBatch);
 
-        /// <summary>
-        /// Removes a game object.
-        /// </summary>
-        public void Destroy();
+    /// <summary>
+    /// Removes a game object.
+    /// </summary>
+    public void Destroy();
 }
