00d44534070d0863710e4fc74fe0e5ce4807ade9
Hvemslev <111503814+Hvemslev@users.noreply.github.com>
2026-04-06 16:04:29 +0200

    attempted to overhaul player movement to allow dash (WIP)

--- SPL2_Project/Player.cs
+++ SPL2_Project/Player.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework.Input;
 
 namespace SPL2_Project;
@@ -7,28 +8,44 @@ public class Player
 {
     public Vector2 position = new Vector2(100, 100);
 
+    
+
     private int playerSpeed = 4;
 
     public void Update()
     {
-        if (Keyboard.GetState().IsKeyDown(Keys.W))
-        {
-            position.Y -= playerSpeed;
-        }
+        //Vector2 movementDirection = new Vector2(0,0);
+        Vector2 movement = new Vector2(0,0);
+        //movement = Vector2.Normalize(movementDirection);
+        
+        
 
-        if (Keyboard.GetState().IsKeyDown(Keys.S))
-        {
-            position.Y += playerSpeed;
-        }
 
-        if (Keyboard.GetState().IsKeyDown(Keys.A))
+        if(Keyboard.GetState().IsKeyDown(Keys.W))
         {
-            position.X -= playerSpeed;
-        }
+            movement.Y=(-1);
+        } 
+        else if(Keyboard.GetState().IsKeyDown(Keys.S))
+        {
+            movement.Y=1;
+        } 
+        else {movement.Y=0;}
 
-        if (Keyboard.GetState().IsKeyDown(Keys.D))
+        if(Keyboard.GetState().IsKeyDown(Keys.A))
         {
-            position.X += playerSpeed;
-        }
+            movement.X=(-1);
+        } 
+        else if(Keyboard.GetState().IsKeyDown(Keys.D))
+        {
+            movement.X=1;
+        } else {movement.X=0;}
+
+
+
+
+        position+=movement*playerSpeed;
+        
+
+        
     }
 }
